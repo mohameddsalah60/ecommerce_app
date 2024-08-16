90369a513a38a35c2018c619a617aa30cb6a01b2
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-08-17 02:55:09 +0300

    finish splash view logic

---
+++ lib/features/auth/presentetion/views/sign_in_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignInView extends StatelessWidget {
+  const SignInView({super.key});
+  static const routeName = 'signIn';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/features/splash/presentetion/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentetion/views/widgets/splash_view_body.dart
@@ -1,8 +1,20 @@
 import 'package:ecommerce_app/core/utils/app_images.dart';
+import 'package:ecommerce_app/features/auth/presentetion/views/sign_in_view.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    navigateToHome();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +24,17 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(Assets.imagesAppLogo),
       ],
+    );
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Navigator.of(context).pushReplacementNamed(
+          SignInView.routeName,
+        );
+      },
     );
   }
 }
