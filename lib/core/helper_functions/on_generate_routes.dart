e48775e996a35bbabf957963c76c0d7bbdf41c28
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2025-01-25 21:04:15 +0200

    create new address view

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -5,6 +5,7 @@ import 'package:ecommerce_app/features/home/presentetion/views/main_view.dart';
 import 'package:ecommerce_app/features/splash/presentetion/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
+import '../../features/addresses/presentation/views/new_address_view.dart';
 import '../../features/categories/presentation/views/categories_details_view.dart';
 import '../../features/home/presentetion/views/cart_view.dart';
 import '../entites/product_entity.dart';
@@ -41,6 +42,9 @@ Route<dynamic> onGenerateRoutes(RouteSettings routeSettings) {
       );
     case CartView.routeName:
       return MaterialPageRoute(builder: (context) => const CartView());
+    case NewAddressView.routeName:
+      return MaterialPageRoute(builder: (context) => const NewAddressView());
+
     case MainView.routeName:
       return MaterialPageRoute(builder: (context) => const MainView());
     default:
---
+++ lib/features/addresses/presentation/views/new_address_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewAddressView extends StatelessWidget {
+  const NewAddressView({super.key});
+  static const routeName = 'newAddress';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
